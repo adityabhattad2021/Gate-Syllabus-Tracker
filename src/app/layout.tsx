04ebde0e9a9afd5fc3fd0f7ed364e3ea0c1d3944
Aditya Bhattad <adityabhattad18@gmail.com>
2024-08-31 17:25:55 +0530

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -40,6 +41,7 @@ export default function RootLayout({
         <main className="flex-grow">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
