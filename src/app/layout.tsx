37fb559aa86f49927ce369910ac680b88339f810
Aditya Bhattad <adityabhattad18@gmail.com>
2024-08-31 17:02:59 +0530

    feat: add proper metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,28 @@ import Navbar from "@/components/navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SyllabiGenius",
-  description: "Track your syllabus and prepare for your exams with ease",
-};
-
+  title: "SyllabiGenius | Track Your Syllabus and Prepare for Exams",
+  description: "SyllabiGenius  aims to simplify exam preparation by providing an intuitive interface for managing study materials and monitoring progress for various exams starting from GATE.",
+  keywords: ["syllabus tracker", "exam preparation", "study planner", "academic organizer", "student tool","Gate 2025","Gate 2026","GATE 2025","GATE 2026"],
+  authors: [{ name: "Aditya Bhattad" }],
+  openGraph: {
+    title: "SyllabiGenius | Your Ultimate Study Companion",
+    description: "Track your syllabus, manage your study schedule, and ace your exams with SyllabiGenius.",
+    url: "https://syllabigenius.vercel.app/",
+    siteName: "SyllabiGenius",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SyllabiGenius | Track Your Syllabus and Prepare for Exams",
+    description: "SyllabiGenius  aims to simplify exam preparation by providing an intuitive interface for managing study materials and monitoring progress for various exams starting from GATE.",
+    creator: "@bhattad_aditya",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+}
 export default function RootLayout({
   children,
 }: Readonly<{
