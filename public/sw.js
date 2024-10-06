fb9a056f78a8deacc4ec10a24cd842481304973b
Aditya Bhattad <adityabhattad18@gmail.com>
2024-10-06 23:59:48 +0530

    feat: implement caching the static data

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
+const ASSETS_CACHE_NAME = 'assets-cache-v1';
+
+const ASSETS_TO_CACHE = [
+    "_next/static"
+]
+
+
 self.addEventListener('install', (event) => {
     console.log('Service Worker installing.');
 });
@@ -8,5 +15,21 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     console.log('Fetching:', event.request.url);
-    event.respondWith(fetch(event.request));
+    // Check if the request is for a file in the _next/static directory
+    if (event.request.url.includes('_next/static')) {
+        event.respondWith(
+            (async () => {
+                const cachedResponse = await caches.match(event.request);
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+                const fetchResponse = await fetch(event.request);
+                const cache = await caches.open(ASSETS_CACHE_NAME);
+                await cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+            })()
+        );
+    } else {
+        event.respondWith(fetch(event.request));
+    }
 });
